c2ed73d06b6cdd5f8156367f57045bc49a0de9d1
Hieu <trunghieubui88@gmail.com>
2023-03-30 08:34:36 +0700

    init deploy

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/0xk2/twitter-endpoint/handler"
 )
 
 func main() {
+	port := os.Getenv("PORT")
 	http.HandleFunc("/verify", handler.VerifyHandler)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":"+port, nil)
 }
